afcd682823a566f1756ddb2b7f22f4af75b3a35c
damon <pairwinter@126.com>
2013-07-08 11:13:23 +0800

    update

--- nodejs/test/index.js
+++ nodejs/test/index.js
@@ -2,6 +2,7 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 var handle = {}
+//下面三个请求最终都将因为超时而无法响应，因为他们都没有对Response进行写操作，并且没有结束写操作。
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
--- nodejs/test/requestHandlers.js
+++ nodejs/test/requestHandlers.js
@@ -1,3 +1,5 @@
+//最终都将因为超时而无法响应，因为他们都没有对Response进行写操作，并且没有结束写操作。
+//参数中包含Response但是这里没有处理。
 function start() {
 	console.log("Request handler 'start' was called.");
 	function sleep(milliSeconds) {
@@ -6,10 +8,22 @@ function start() {
 	}
 	sleep(10000);
 	return "Hello Start";
+    //正确的方式如下
+    var response = arguments[0];
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write("Hello Start");
+    response.end();
 }
+//最终都将因为超时而无法响应，因为他们都没有对Response进行写操作，并且没有结束写操作。
+//参数中包含Response但是这里没有处理。
 function upload() {
 	console.log("Request handler 'upload' was called.");
 	return "Hello Upload";
+    //正确的方式如下
+//    var response = arguments[0];
+//    response.writeHead(200, {"Content-Type" : "text/html"});
+//    response.write("Hello Start");
+//    response.end();
 }
 exports.start = start;
 exports.upload = upload;
