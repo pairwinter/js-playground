970e9903dcee7204d60f64345811ede7e6f11e31
damon <damon.liu@everbridge.com>
2013-08-21 15:34:07 +0800

    update

--- js-course/node/routes/index.js
+++
@@ -1,8 +0,0 @@
-
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Javascript Course',layout:false});
-};
---
+++ js-course/node_app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/everbridge'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.listen(8080);
+console.log('Listening on port 8080');
