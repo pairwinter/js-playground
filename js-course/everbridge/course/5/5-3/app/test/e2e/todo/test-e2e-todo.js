897a1cd0c371191a7ee8c34ceb90411297b93a77
damon <damon.liu@everbridge.com>
2013-08-30 18:28:26 +0800

    update

--- js-course/everbridge/course/5/5-3/app/test/e2e/todo/test-e2e-todo.js
+++ js-course/everbridge/course/5/5-3/app/test/e2e/todo/test-e2e-todo.js
@@ -2,10 +2,13 @@
 
 describe("Test Todo", function(){
   beforeEach(function(){
-    browser().navigateTo("/course/5/5-3/app/views/main.html#todo");
+    browser().navigateTo("/course/5/5-3/app/views/main.html#/todo");
     sleep(1);
+//    browser().window().hash("todo");
+//    sleep(1);
   });
   it("Should filter results", function(){
+    console.log(browser().location().path());
     input("todo").enter("jacksparrow");
     element(":button").click();
     expect(repeater("ul li").count()).toEqual(1);
