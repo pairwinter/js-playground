84d528e3be8f10f86f932d294c60d4425816c4fc
damon <damon.liu@everbridge.com>
2013-11-28 09:17:15 +0800

    update test.js

--- settings/test/spec/organization/test.js
+++ settings/test/spec/organization/test.js
@@ -1,7 +1,6 @@
 'use strict';
 
-describe('Test', function () {
-
+xdescribe('Test', function () {
     it('should attach a list of todo itmes to the scope', function () {
         expect([1, 2, 3].length).toBe(3);
     });
